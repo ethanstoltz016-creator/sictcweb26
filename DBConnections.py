d542e3e0a2719a7cb77b0ee7b8b50057cef1e21a
ethanstoltz016-creator <ethan.stoltz016@gmail.com>
2026-04-14 09:56:30 -0500

    4/14

--- DBConnections.py
+++ DBConnections.py
@@ -1,20 +1,26 @@
 import pymysql
 
-#global stuff
-user="sictc"
-password="Pencil1"
-dbName = "IoT"
-host = "localhost"
+#function
+def connectToDB():
+    user = "sictc"
+    password = "Pencil1"
+    dbName = "IoT"
+    host = "localhost"
 
-connection = pymysql.connect(
-    database=dbName,
-    user=user,
-    password=password,
-    host=host
-)
-print(connection)
-print(dir(connection))
+    connection = pymysql.connect(
+        database=dbName,
+        user=user,
+        password=password,
+        host=host
+    )
+    #print(connection)
+    #print(dir(connection))
 
-#functions
-
-#mainloop
+    #mainloop
+    try:
+        if connection:
+            print("Connected to the db")
+    except pymysql.Error as e:
+        print(f"Error while connecting to db: {e}")
+        
+connectToDB()
--- routes/States.py
+++ routes/States.py
@@ -2,13 +2,21 @@ from flask import Flask, request, redirect, url_for, jsonify, Blueprint
 #import pymysql
 #import os
 #from dotenv import load_dotenv
+from sictcweb26.DBConnections import connectToDB
 
 # global var and obj
 state_bp = Blueprint('state_bp', __name__)
+dbc = connectToDB()
 
 #functions
 @state_bp.route("/welcome")
 def welcome():
     return "Hello and welcome to the States route"
 
-#mainloop - not needed
+#GET/READ every record from States Table
+@state_bp.route('/')
+def getAll():
+    queryString = f"SELECT * FROM States"   #!!! NOTICE NO ;
+    print(queryString) #debug purposes
+    print(dbc)
+    return "get all"
